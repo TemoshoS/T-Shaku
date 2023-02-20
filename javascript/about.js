f6148c06b0b0f8829e02c476552018052ab3a6ed
Temosho shaku <52781030+TemoshoS@users.noreply.github.com>
2023-02-20 12:01:05 +0200

    Add files via upload

---
+++ javascript/about.js
@@ -0,0 +1,27 @@
+document.getElementById("name").innerHTML = "Temosho shaku";
+
+document.getElementById("bio").innerHTML = "A highly motivated software developer seeking to get a position in a reputed company, where I can use my skills and knowledge to learn new things and grow as a software developer.";
+
+document.getElementById("education").innerHTML = "Education";
+
+document.getElementById("school").innerHTML = "Tshwane University of Technology";
+document.getElementById("major").innerHTML = "Major Subjects";
+document.getElementById("modules").innerHTML = "<li>Development Software </li><li> Information System </li><li> Technical Programming </li><li> System Software";
+
+document.getElementById("secondary").innerHTML = "Mokhulwane Seconday School";
+document.getElementById("subjects").innerHTML = "<li>Mathematics</li><li>Life Science </li><li> Physical Science </li><li> Agricultural Science";
+
+
+document.getElementById("skill").innerHTML = "Skills";
+document.getElementById("skills").innerHTML = "<li>Angular</li> <li>Azure</li> <li>HTML, CSS and JavaScript</li> <li>Java and c#</li> <li>SQL</li> <li>Git</li><li>Web design and development</li> <li>Problem solving and logical thinking</li> <li>Teamworker</li> <li>Excellent interpersonal skills</li>";
+
+document.getElementById("experience").innerHTML = "Experience";
+document.getElementById("experiences").innerHTML = "<h2>Dynamic dna - Microsoft Azure learnership</h2><p><b>March 2021-June 2022</b></p><li>Quickly learned new skills and applied them to daily tasks</li><li>Used planning skills to achieve results according to schedule</li><li>Used critical thinking to down problems, evaluate solutions and make decisions</li><li>Learned azure concepts</li><li>Learned to design and develop web applications using c# and ASP.NET</li><h2>Deviare - Angular 8 learnership</h2><p><b>August 2020-Septermber 2020</b></p><li>Designed and developed a web application using angular and JSON server.</li><li>Designing and developing user interfaces using angular 8 best practices</li><li>Writing JavaScript, CSS, and HTML</li><li>Writing clean code</li><li>Ensuring high performance.</li>";
+
+
+document.getElementById("project").innerHTML = "Projects";
+document.getElementById("unfinishedprojects").innerHTML = "<li> File Management System</li><li> Que Management System<li> Food blog app</li> <li> Ecommerce website</li>";
+
+
+document.getElementById("reference").innerHTML = "References";
+document.getElementById("reference1").innerHTML = "<li>Available on request</li>";
